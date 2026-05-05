7502abb3c69c8d5a9ef127dcfed64ef0719e60f1
art674974 <arthur2802.a@gmail.com>
2026-05-05 20:53:47 -0300

    fix: atualização do secret dentro do codigo

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+from dotenv import load_dotenv
 from pathlib import Path
+import os 
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!gs-1$8gi5mv%)p$204p%s97mdx8vw1xpg#sbv1#@^19y4r3sp'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("Debug")
 
 ALLOWED_HOSTS = []
 
